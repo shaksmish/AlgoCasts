66db4de75756561d5247a6772ae0d944770f1506
Shalki Mishra <shaksmish@users.noreply.github.com>
2023-08-04 18:26:46 +0200

    updated solution 3

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -15,14 +15,23 @@
 // }
 
 // Solution 2
+// function reverseInt(n) {
+//     const reversed = n.toString().split('').reverse().join('');
+
+//     if (n<0) {
+//         return parseInt(reversed) * -1;
+//     }
+
+//     return parseInt(reversed);
+    
+// }
+
+// Solution 3
 function reverseInt(n) {
     const reversed = n.toString().split('').reverse().join('');
+        
+    return parseInt(reversed) * Math.sign(n);
 
-    if (n<0) {
-        return parseInt(reversed) * -1;
-    }
-    
-    return parseInt(reversed);
     
 }
 
