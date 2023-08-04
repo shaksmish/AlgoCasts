74b9c43fee20634ec5bb54d83ddefb9b38263911
Shalki Mishra <shaksmish@users.noreply.github.com>
2023-08-04 17:38:41 +0200

    update solution 2 to palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,13 +7,28 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {
-    let newStr = "";
-    newStr = str.split('').reverse().join('');
 
-    if (str === newStr) {
-        return true;
-    } 
-}
+// Solution 1
+// function palindrome(str) {
+//     let newStr = "";
+//     newStr = str.split('').reverse().join('');
+
+//     if (str === newStr) {
+//         return true;
+//     } 
+// }
+
+//Solution 2 using array.prototype.every() method, we check each character in the array to see if it matches the character in the opposite position in the array.
+// this method can be run till the middle of the string/array, and accordingly declared if the string is palindrome or not
+// not ideal solution
+
+// function palindrome(str) {
+//     return str.split('').every((character, i) => {
+//     return character === str[str.length - i - 1];
+//  });
+
+// }
+
+
 
 module.exports = palindrome;
